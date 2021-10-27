017b085ac95671f2e67cefe4b79117157d5f63b4
chenduoru <daychanhk@gmail.com>
2021-10-27 11:31:26 +0800

    cdr update github workflow

---
+++ build_deps.sh
@@ -0,0 +1,26 @@
+cd ${{github.workspace}}/deps
+cd libevent
+git checkout release-2.1.12-stable
+mkdir build
+cd build
+cmake .. -DEVENT__DISABLE_OPENSSL=ON
+make
+sudo make install
+
+cd ${{github.workspace}}/deps
+cd googletest
+mkdir build
+cd build
+cmake ..
+make
+sudo make install
+
+cd ${{github.workspace}}/deps
+cd jsoncpp
+mkdir build
+cd build
+cmake -DJSONCPP_WITH_TESTS=OFF -DJSONCPP_WITH_POST_BUILD_UNITTEST=OFF ..
+make
+sudo make install
+
+cd ${{github.workspace}}
