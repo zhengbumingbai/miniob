ba3ada1f027145da07e0f71f48083e03f7c7379b
chenduoru <daychanhk@gmail.com>
2021-10-27 11:46:44 +0800

    cdr update github workflow

--- build_deps.sh
+++ build_deps.sh
@@ -7,7 +7,7 @@ git checkout release-2.1.12-stable
 mkdir build
 cd build
 cmake .. -DEVENT__DISABLE_OPENSSL=ON
-make
+make -j16
 sudo make install
 cd ../..
 
@@ -16,7 +16,7 @@ cd googletest
 mkdir build
 cd build
 cmake ..
-make
+make -j16
 sudo make install
 cd ../..
 
@@ -25,6 +25,6 @@ cd jsoncpp
 mkdir build
 cd build
 cmake -DJSONCPP_WITH_TESTS=OFF -DJSONCPP_WITH_POST_BUILD_UNITTEST=OFF ..
-make
+make -j16
 sudo make install
 cd ../..
